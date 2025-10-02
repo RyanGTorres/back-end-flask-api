1d743a721592db4d3de41f3a47d4d361309fa116
RyanGTorres <ryryan009@gmail.com>
2025-10-02 17:16:12 -0300

    feat: implementa JWT, ajustes no banco de dados e adição de logs

--- app/models/user.py
+++ app/models/user.py
@@ -8,8 +8,9 @@ class User(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    cpf = db.Column(db.String(20), unique=True, nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
     
     def __repr__(self):
         return f'<User {self.username}>'
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,8 +1,10 @@
 from app import db
 from app.models.user import User
 from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
+from typing import Optional
+from uuid import UUID
 import logging
-
 
 
 class UserService:
@@ -16,23 +18,26 @@ class UserService:
             error_out=False).items
     
     @staticmethod
-    def get_user_by_id(user_id):
+    def get_user_by_id(user_id: UUID)-> Optional[User]:
         """Busca usuario pelo id"""
         return User.query.get(user_id)
     
     @staticmethod
-    def get_user_by_email(user_email):
+    def get_user_by_email(user_email: str)-> Optional[User]:
         """Retorna usuario pelo email"""
         return User.query.filter_by(email=user_email).first()
     
     @staticmethod
-    def create_user(username, email, cpf):
+    def create_user(username, password, email, cpf):
         """Cria um novo usuario"""
         if UserService.get_user_by_email(email):
             raise ValueError("Email já cadastrado!")
         
-        new_user = User(username=username, email=email, cpf=cpf)
-
+        new_user = User(
+            username=username,
+            password= generate_password_hash(password), 
+            email=email, 
+            cpf=cpf)
         try:
             db.session.add(new_user)
             db.session.commit()
@@ -42,7 +47,7 @@ class UserService:
             raise ValueError("Erro ao criar usuário: dados duplicados")
         
     @staticmethod
-    def update_user(user_id, username=None, email=None, cpf=None):
+    def update_user(user_id, username=None, password=None, email=None, cpf=None):
         """Atualiza um usuario existente"""
         user = UserService.get_user_by_id(user_id)
 
@@ -51,17 +56,18 @@ class UserService:
         
         if username is not None:
             user.username = username
+        if password is not None:
+            user.password = generate_password_hash(password)
         if email is not None:
             user.email = email
-        if cpf is not None:
-            user.cpf = cpf
-        
+
+
         try:
             db.session.commit()
             return user
         except IntegrityError as e:
             db.session.rollback()
-            raise ValueError(f"Erro ao atualizar usuário: {str(e)}")
+            raise ValueError(f"Erro ao atualizar usuário",str(e))
     
     @staticmethod
     def delete_user(user_id):
@@ -73,3 +79,12 @@ class UserService:
         db.session.delete(user)
         db.session.commit()
         return True
+    
+    @staticmethod
+    def verify_login_user(email, password):
+        """Verifica as credenciais do usuario"""
+        user = UserService.get_user_by_email(email)
+        if not user or not check_password_hash(user.password, password):
+            raise ValueError("Credenciais inválidas")
+        return user
+       
